dcf1d2a5ee2abfd6bf03f5ddbac1f6fc5b88fa35
Perry Xie <perryxie.nz@gmail.com>
2024-07-13 10:25:18 +1200

    Slider now updates when song is playing.

--- scripts/song.js
+++ scripts/song.js
@@ -1,5 +1,5 @@
 
-const range = document.getElementById('progress');
+let range = document.getElementById("progress");
 
 function updateRange() {
 const value = (range.value - range.min) / (range.max - range.min) * 100;
@@ -8,4 +8,31 @@ range.style.background = `linear-gradient(to right, var(--secondary) 0%, var(--s
 
 updateRange();
 
-range.addEventListener('input', updateRange);
+range.addEventListener("input", updateRange);
+
+let song = document.getElementById("song");
+let crtlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+    if (crtlIcon.classList.contains("fa-pause")){
+        song.pause();
+        crtlIcon.classList.remove("fa-pause");
+        crtlIcon.classList.add("fa-play");
+    } else {
+        song.play();
+        crtlIcon.classList.add("fa-pause");
+        crtlIcon.classList.remove("fa-play");
+    }
+}
+
+if(song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+        updateRange();
+    }, 500)
+}
